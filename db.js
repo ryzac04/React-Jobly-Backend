6ea58f75eeee43a3e89e5310f795bf163ba4caa3
ryzac <rzc044@gmail.com>
2024-05-01 18:26:10 -0500

    Changed db.js

--- db.js
+++ db.js
@@ -7,6 +7,8 @@ let DB_URI;
 
 if (process.env.NODE_ENV === "test") {
   DB_URI = "postgresql:///jobly_test";
+} else if (process.env.DATABASE_URL) {
+  DB_URI = process.env.DATABASE_URL
 } else {
   DB_URI = "postgresqll:///jobly";
 }
